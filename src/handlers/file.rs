75ac99b70e0427765fde5925951c3022b86a57ab
Dmytro Milinevskyy <milinevskyy@gmail.com>
2017-04-03 02:51:06 +0200

    handlers:file: get rid of bogus arc and refcell

--- src/handlers/file.rs
+++ src/handlers/file.rs
@@ -2,8 +2,6 @@ use std::fs::{OpenOptions, create_dir_all};
 use std::sync::Mutex;
 use std::path::Path;
 use std::io::Write;
-use std::cell::RefCell;
-use std::sync::Arc;
 
 use logger::Handler;
 
@@ -12,17 +10,15 @@ pub fn handler(path: &Path) -> Handler<'static> {
         Some(dir) => create_dir_all(dir).unwrap(),
         None => {},
     };
-    let file = Arc::new(Mutex::new(RefCell::new(
+    let file = Mutex::new(
         OpenOptions::new()
             .append(true)
             .create(true)
             .open(path)
             .unwrap()
-    )));
+    );
     Box::new(move |record| {
-        let file = file.clone();
-        let file = file.lock().unwrap();
-        let mut file = file.borrow_mut();
+        let mut file = file.lock().unwrap();
         file.write(record.formatted().as_bytes()).unwrap();
     })
 }
